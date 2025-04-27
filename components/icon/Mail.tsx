053a31dac4a0f07a27e09c251f6f822b0bcefc1b
Ashley Wu <pattywpc@gmail.com>
2025-04-27 23:31:17 +0800

    Add contact icon components

---
+++ components/icon/Mail.tsx
@@ -0,0 +1,13 @@
+import { Mail as MailIcon } from "lucide-react"
+
+function Mail() {
+  return (
+    <span className="p-1.25 bg-primary rounded-full text-white">
+      <MailIcon size={18} />
+    </span>
+  )
+}
+
+export {
+  Mail
+}
---
+++ components/icon/Phone.tsx
@@ -0,0 +1,13 @@
+import { Phone as PhoneIcon } from "lucide-react"
+
+function Phone() {
+  return (
+    <span className="p-1.5 bg-primary rounded-full text-white">
+      <PhoneIcon size={16} />
+    </span>
+  )
+}
+
+export {
+  Phone
+}
